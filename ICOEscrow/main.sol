5c87c5f241fce0bed1286236940546e32c3f8f9d
DenisKaizer <1kaizerd@gmail.com>
2017-12-26 01:00:17 +0300

    ArgumentChecker

---
+++ ICOEscrow/main.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.4.15;
+
+
+contract Stateful {
+  enum State {
+  Init,
+  PreIco,
+  preIcoFinished,
+  ICO,
+  CrowdsaleFinished
+  }
+  State public state = State.Init;
+
+  event StateChanged(State oldState, State newState);
+
+  function setState(State newState) internal {
+    State oldState = state;
+    state = newState;
+    StateChanged(oldState, newState);
+  }
+}
+
+
+contract main {
+
+  modifier onlyAtVoting() {
+
+  }
+
+  function approveRequest() onlyAtVoting {
+
+  }
+
+  function declineRequest() onlyAtVoting {
+
+  }
+
+  function requestMoney(address _to) {
+
+  }
+
+  function () payable {}
+}
